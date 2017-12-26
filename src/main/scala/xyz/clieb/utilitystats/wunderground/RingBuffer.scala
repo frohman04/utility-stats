9ba82a325063df1749fa89cf56030b4369a8dbd6
Chris Lieb <clieb@bitsighttech.com>
2017-12-26 14:58:29 -0500

    Fix RingBuffer implementation

--- src/main/scala/xyz/clieb/utilitystats/wunderground/RingBuffer.scala
+++ src/main/scala/xyz/clieb/utilitystats/wunderground/RingBuffer.scala
@@ -1,15 +1,19 @@
 package xyz.clieb.utilitystats.wunderground
 
+import com.typesafe.scalalogging.LazyLogging
+
 import scala.collection.mutable
 
-private[wunderground] class RingBuffer[T](size: Int) {
+private[wunderground] class RingBuffer[T](size: Int) extends LazyLogging {
   private val buffer = mutable.IndexedSeq.fill[T](size)(null.asInstanceOf[T])
   private var curr = 0
+  private var next = 1
 
   def add(value: T): Unit = {
-    curr = (curr + 1) % buffer.length
+    curr = next
+    next = (curr + 1) % buffer.length
     buffer(curr) = value
   }
 
-  def peekTail: T = buffer(curr)
+  def peekTail: T = buffer(next)
 }
