17606a079eb40ada46e0c545605f3906c6c15a4f
Chris Lieb <chris.lieb@gmail.com>
2019-03-03 14:53:17 -0500

    Split DataPoint into currently, minutely, hourly, and daily

--- rust/src/darksky.rs
+++ rust/src/darksky.rs
@@ -12,13 +12,13 @@ struct DarkSkyResponse {
     /// certainly result in Daylight Saving Time bugs. Please use timezone, instead.)
     pub offset: i8,
     /// A data point containing the current weather conditions at the requested location.
-    pub currently: Option<DataPoint>,
+    pub currently: Option<DataPointCurrently>,
     /// A data block containing the weather conditions minute-by-minute for the next hour.
-    pub minutely: Option<DataBlock>,
+    pub minutely: Option<DataBlock<DataPointMinutely>>,
     /// A data block containing the weather conditions hour-by-hour for the next two days.
-    pub hourly: Option<DataBlock>,
+    pub hourly: Option<DataBlock<DataPointHourly>>,
     /// A data block containing the weather conditions day-by-day for the next week.
-    pub daily: Option<DataBlock>,
+    pub daily: Option<DataBlock<DataPointDaily>>,
     /// An alerts array, which, if present, contains any severe weather alerts pertinent to the
     /// requested location.
     pub alerts: Option<Vec<Alert>>,
@@ -28,10 +28,10 @@ struct DarkSkyResponse {
 
 /// A data block object represents the various weather phenomena occurring over a period of time.
 #[derive(Debug, Serialize, Deserialize)]
-struct DataBlock {
+struct DataBlock<DP> {
     /// An array of data points, ordered by time, which together describe the weather conditions at
     /// the requested location over time.
-    pub data: Vec<DataPoint>,
+    pub data: Vec<DP>,
     /// A human-readable summary of this data block.
     pub summary: Option<String>,
     /// A machine-readable text summary of this data block. (May take on the same values as the icon
@@ -43,7 +43,229 @@ struct DataBlock {
 /// specified) of a particular weather phenomenon occurring during a period of time: an instant in
 /// the case of currently, a minute for minutely, an hour for hourly, and a day for daily
 #[derive(Debug, Serialize, Deserialize)]
-struct DataPoint {
+struct DataPointCurrently {
+    /// The UNIX time at which this data point begins. minutely data point are always aligned to the
+    /// top of the minute, hourly data point objects to the top of the hour, and daily data point
+    /// objects to midnight of the day, all according to the local time zone.
+    pub time: i64,
+    /// A human-readable text summary of this data point. (This property has millions of possible
+    /// values, so don’t use it for automated purposes: use the icon property, instead!)
+    pub summary: Option<String>,
+    /// A machine-readable text summary of this data point, suitable for selecting an icon for
+    /// display. If defined, this property will have one of the following values: clear-day,
+    /// clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or
+    /// partly-cloudy-night. (Developers should ensure that a sensible default is defined, as
+    /// additional values, such as hail, thunderstorm, or tornado, may be defined in the future.)
+    pub icon: Option<String>,
+    /// The intensity (in inches of liquid water per hour) of precipitation occurring at the given
+    /// time. This value is conditional on probability (that is, assuming any precipitation occurs
+    /// at all).
+    #[serde(alias = "precipIntensity")]
+    pub precip_intensity: Option<f32>,
+    /// The standard deviation of the distribution of precipIntensity. (We only return this property
+    /// when the full distribution, and not merely the expected mean, can be estimated with
+    /// accuracy.)
+    #[serde(alias = "precipIntensityError")]
+    pub precip_intensity_error: Option<f32>,
+    /// The probability of precipitation occurring, between 0 and 1, inclusive.
+    #[serde(alias = "precipProbability")]
+    pub precip_probability: Option<f32>,
+    /// The dew point in degrees Fahrenheit.
+    #[serde(alias = "dewPoint")]
+    pub dew_point: Option<f32>,
+    /// The relative humidity, between 0 and 1, inclusive.
+    pub humidity: Option<f32>,
+    /// The sea-level air pressure in millibars.
+    pub pressure: Option<f32>,
+    /// The wind speed in miles per hour.
+    #[serde(alias = "windSpeed")]
+    pub wind_speed: Option<f32>,
+    /// The wind gust speed in miles per hour.
+    #[serde(alias = "windGust")]
+    pub wind_gust: Option<f32>,
+    /// The time at which the maximum wind gust speed occurs during the day.
+    #[serde(alias = "windGustTime")]
+    pub wind_gust_time: Option<u64>,
+    /// The direction that the wind is coming from in degrees, with true north at 0° and progressing
+    /// clockwise. (If windSpeed is zero, then this value will not be defined.)
+    #[serde(alias = "windBearing")]
+    pub wind_bearing: Option<i16>,
+    /// The percentage of sky occluded by clouds, between 0 and 1, inclusive.
+    #[serde(alias = "cloudCover")]
+    pub cloud_cover: Option<f32>,
+    /// The UV index.
+    #[serde(alias = "uvIndex")]
+    pub uv_index: Option<u8>,
+    /// The average visibility in miles, capped at 10 miles.
+    pub visibility: Option<f32>,
+    /// The approximate direction of the nearest storm in degrees, with true north at 0° and
+    /// progressing clockwise. (If nearestStormDistance is zero, then this value will not be
+    /// defined.) (only on currently)
+    #[serde(alias = "nearestStormBearing")]
+    pub nearest_storm_bearing: Option<i16>,
+    /// The approximate distance to the nearest storm in miles. (A storm distance of 0 doesn’t
+    /// necessarily refer to a storm at the requested location, but rather a storm in the vicinity
+    /// of that location.) (only on currently)
+    #[serde(alias = "nearestStormDistance")]
+    pub nearest_storm_distance: Option<f32>,
+    /// The columnar density of total atmospheric ozone at the given time in Dobson units.
+    pub ozone: Option<f32>,
+}
+
+/// A data point object contains various properties, each representing the average (unless otherwise
+/// specified) of a particular weather phenomenon occurring during a period of time: an instant in
+/// the case of currently, a minute for minutely, an hour for hourly, and a day for daily
+#[derive(Debug, Serialize, Deserialize)]
+struct DataPointMinutely {
+    /// The UNIX time at which this data point begins. minutely data point are always aligned to the
+    /// top of the minute, hourly data point objects to the top of the hour, and daily data point
+    /// objects to midnight of the day, all according to the local time zone.
+    pub time: i64,
+    /// A human-readable text summary of this data point. (This property has millions of possible
+    /// values, so don’t use it for automated purposes: use the icon property, instead!)
+    pub summary: Option<String>,
+    /// A machine-readable text summary of this data point, suitable for selecting an icon for
+    /// display. If defined, this property will have one of the following values: clear-day,
+    /// clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or
+    /// partly-cloudy-night. (Developers should ensure that a sensible default is defined, as
+    /// additional values, such as hail, thunderstorm, or tornado, may be defined in the future.)
+    pub icon: Option<String>,
+    /// The intensity (in inches of liquid water per hour) of precipitation occurring at the given
+    /// time. This value is conditional on probability (that is, assuming any precipitation occurs
+    /// at all).
+    #[serde(alias = "precipIntensity")]
+    pub precip_intensity: Option<f32>,
+    /// The standard deviation of the distribution of precipIntensity. (We only return this property
+    /// when the full distribution, and not merely the expected mean, can be estimated with
+    /// accuracy.)
+    #[serde(alias = "precipIntensityError")]
+    pub precip_intensity_error: Option<f32>,
+    /// The probability of precipitation occurring, between 0 and 1, inclusive.
+    #[serde(alias = "precipProbability")]
+    pub precip_probability: Option<f32>,
+    /// The type of precipitation occurring at the given time. If defined, this property will have
+    /// one of the following values: "rain", "snow", or "sleet" (which refers to each of freezing
+    /// rain, ice pellets, and “wintery mix”). (If precipIntensity is zero, then this property will
+    /// not be defined. Additionally, due to the lack of data in our sources, historical precipType
+    /// information is usually estimated, rather than observed.)
+    #[serde(alias = "precipType")]
+    pub precip_type: Option<String>,
+    /// The dew point in degrees Fahrenheit.
+    #[serde(alias = "dewPoint")]
+    pub dew_point: Option<f32>,
+    /// The relative humidity, between 0 and 1, inclusive.
+    pub humidity: Option<f32>,
+    /// The sea-level air pressure in millibars.
+    pub pressure: Option<f32>,
+    /// The wind speed in miles per hour.
+    #[serde(alias = "windSpeed")]
+    pub wind_speed: Option<f32>,
+    /// The wind gust speed in miles per hour.
+    #[serde(alias = "windGust")]
+    pub wind_gust: Option<f32>,
+    /// The time at which the maximum wind gust speed occurs during the day.
+    #[serde(alias = "windGustTime")]
+    pub wind_gust_time: Option<u64>,
+    /// The direction that the wind is coming from in degrees, with true north at 0° and progressing
+    /// clockwise. (If windSpeed is zero, then this value will not be defined.)
+    #[serde(alias = "windBearing")]
+    pub wind_bearing: Option<i16>,
+    /// The percentage of sky occluded by clouds, between 0 and 1, inclusive.
+    #[serde(alias = "cloudCover")]
+    pub cloud_cover: Option<f32>,
+    /// The UV index.
+    #[serde(alias = "uvIndex")]
+    pub uv_index: Option<u8>,
+    /// The average visibility in miles, capped at 10 miles.
+    pub visibility: Option<f32>,
+    /// The columnar density of total atmospheric ozone at the given time in Dobson units.
+    pub ozone: Option<f32>,
+}
+
+/// A data point object contains various properties, each representing the average (unless otherwise
+/// specified) of a particular weather phenomenon occurring during a period of time: an instant in
+/// the case of currently, a minute for minutely, an hour for hourly, and a day for daily
+#[derive(Debug, Serialize, Deserialize)]
+struct DataPointHourly {
+    /// The UNIX time at which this data point begins. minutely data point are always aligned to the
+    /// top of the minute, hourly data point objects to the top of the hour, and daily data point
+    /// objects to midnight of the day, all according to the local time zone.
+    pub time: i64,
+    /// A human-readable text summary of this data point. (This property has millions of possible
+    /// values, so don’t use it for automated purposes: use the icon property, instead!)
+    pub summary: Option<String>,
+    /// A machine-readable text summary of this data point, suitable for selecting an icon for
+    /// display. If defined, this property will have one of the following values: clear-day,
+    /// clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or
+    /// partly-cloudy-night. (Developers should ensure that a sensible default is defined, as
+    /// additional values, such as hail, thunderstorm, or tornado, may be defined in the future.)
+    pub icon: Option<String>,
+    /// The intensity (in inches of liquid water per hour) of precipitation occurring at the given
+    /// time. This value is conditional on probability (that is, assuming any precipitation occurs
+    /// at all).
+    #[serde(alias = "precipIntensity")]
+    pub precip_intensity: Option<f32>,
+    /// The standard deviation of the distribution of precipIntensity. (We only return this property
+    /// when the full distribution, and not merely the expected mean, can be estimated with
+    /// accuracy.)
+    #[serde(alias = "precipIntensityError")]
+    pub precip_intensity_error: Option<f32>,
+    /// The probability of precipitation occurring, between 0 and 1, inclusive.
+    #[serde(alias = "precipProbability")]
+    pub precip_probability: Option<f32>,
+    /// The type of precipitation occurring at the given time. If defined, this property will have
+    /// one of the following values: "rain", "snow", or "sleet" (which refers to each of freezing
+    /// rain, ice pellets, and “wintery mix”). (If precipIntensity is zero, then this property will
+    /// not be defined. Additionally, due to the lack of data in our sources, historical precipType
+    /// information is usually estimated, rather than observed.)
+    #[serde(alias = "precipType")]
+    pub precip_type: Option<String>,
+    /// The amount of snowfall accumulation expected to occur, in inches. (If no snowfall is
+    /// expected, this property will not be defined.) (only on hourly and daily)
+    #[serde(alias = "precipAccumulation")]
+    pub precip_accumulation: Option<f32>,
+    /// The air temperature in degrees Fahrenheit. (only on hourly)
+    pub temperature: Option<f32>,
+    /// The apparent (or “feels like”) temperature in degrees Fahrenheit. (only on hourly)
+    #[serde(alias = "apparentTemperature")]
+    pub apparent_temperature: Option<f32>,
+    /// The dew point in degrees Fahrenheit.
+    #[serde(alias = "dewPoint")]
+    pub dew_point: Option<f32>,
+    /// The relative humidity, between 0 and 1, inclusive.
+    pub humidity: Option<f32>,
+    /// The sea-level air pressure in millibars.
+    pub pressure: Option<f32>,
+    /// The wind speed in miles per hour.
+    #[serde(alias = "windSpeed")]
+    pub wind_speed: Option<f32>,
+    /// The wind gust speed in miles per hour.
+    #[serde(alias = "windGust")]
+    pub wind_gust: Option<f32>,
+    /// The time at which the maximum wind gust speed occurs during the day.
+    #[serde(alias = "windGustTime")]
+    pub wind_gust_time: Option<u64>,
+    /// The direction that the wind is coming from in degrees, with true north at 0° and progressing
+    /// clockwise. (If windSpeed is zero, then this value will not be defined.)
+    #[serde(alias = "windBearing")]
+    pub wind_bearing: Option<i16>,
+    /// The percentage of sky occluded by clouds, between 0 and 1, inclusive.
+    #[serde(alias = "cloudCover")]
+    pub cloud_cover: Option<f32>,
+    /// The UV index.
+    #[serde(alias = "uvIndex")]
+    pub uv_index: Option<u8>,
+    /// The average visibility in miles, capped at 10 miles.
+    pub visibility: Option<f32>,
+    /// The columnar density of total atmospheric ozone at the given time in Dobson units.
+    pub ozone: Option<f32>,
+}
+
+/// A data point object contains various properties, each representing the average (unless otherwise
+/// specified) of a particular weather phenomenon occurring during a period of time: an instant in
+/// the case of currently, a minute for minutely, an hour for hourly, and a day for daily
+#[derive(Debug, Serialize, Deserialize)]
+struct DataPointDaily {
     /// The UNIX time at which this data point begins. minutely data point are always aligned to the
     /// top of the minute, hourly data point objects to the top of the hour, and daily data point
     /// objects to midnight of the day, all according to the local time zone.
@@ -87,8 +309,6 @@ struct DataPoint {
     /// expected, this property will not be defined.) (only on hourly and daily)
     #[serde(alias = "precipAccumulation")]
     pub precip_accumulation: Option<f32>,
-    /// The air temperature in degrees Fahrenheit. (only on hourly)
-    pub temperature: Option<f32>,
     /// The daytime high temperature. (only on daily)
     #[serde(alias = "temperatureHigh")]
     pub temperature_high: Option<f32>,
@@ -115,9 +335,6 @@ struct DataPoint {
     /// on daily)
     #[serde(alias = "temperatureMinTime")]
     pub temperature_min_time: Option<u64>,
-    /// The apparent (or “feels like”) temperature in degrees Fahrenheit. (only on hourly)
-    #[serde(alias = "apparentTemperature")]
-    pub apparent_temperature: Option<f32>,
     /// The daytime high apparent temperature. (only on daily)
     #[serde(alias = "apparentTemperatureHigh")]
     pub apparent_temperature_high: Option<f32>,
@@ -183,16 +400,6 @@ struct DataPoint {
     /// gibbous, and waning crescent moons, respectively.) (only on daily)
     #[serde(alias = "moonPhase")]
     pub moon_phase: Option<f32>,
-    /// The approximate direction of the nearest storm in degrees, with true north at 0° and
-    /// progressing clockwise. (If nearestStormDistance is zero, then this value will not be
-    /// defined.) (only on currently)
-    #[serde(alias = "nearestStormBearing")]
-    pub nearest_storm_bearing: Option<i16>,
-    /// The approximate distance to the nearest storm in miles. (A storm distance of 0 doesn’t
-    /// necessarily refer to a storm at the requested location, but rather a storm in the vicinity
-    /// of that location.) (only on currently)
-    #[serde(alias = "nearestStormDistance")]
-    pub nearest_storm_distance: Option<f32>,
     /// The columnar density of total atmospheric ozone at the given time in Dobson units.
     pub ozone: Option<f32>,
     /// The UNIX time of when the sun will rise during a given day. (only on daily)
